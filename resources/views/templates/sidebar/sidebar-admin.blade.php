2b5a13c93dd21d728d64e080daefe40d40348a8c
RidhoAldiko <RidhoAldiko@student.uir.ac.id>
2021-04-15 00:18:24 +0700

    memperbaiki icon sidebar

--- resources/views/templates/sidebar/sidebar-admin.blade.php
+++ resources/views/templates/sidebar/sidebar-admin.blade.php
@@ -14,7 +14,7 @@
 
         <li class="menu-header">Menu</li>
         <li class="dropdown">
-        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Pengguna</span></a>
+        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-users"></i> <span>Pengguna</span></a>
         <ul class="dropdown-menu">
             <li><a class="nav-link" href="layout-default.html">Data Pengguna</a></li>
             <li><a class="nav-link" href="layout-transparent.html">Tambah Pengguna</a></li>
