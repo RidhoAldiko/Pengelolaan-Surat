1a48786f7bb053996e79efdb79d8645014ebef9f
RidhoAldiko <RidhoAldiko@student.uir.ac.id>
2021-04-14 15:47:03 +0700

    make multi levels authentication

---
+++ routes/web.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\OperatorSuratController;
+use App\Http\Controllers\OperatorKepegawaianController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//root page (login)
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+//route authentication
+Auth::routes();
+
+//route role 0 = admin
+Route::prefix('admin')
+    ->middleware('auth','role:0')
+    ->group(function(){
+        //admin dashboard
+        Route::get('/', [AdminController::class,'index'])
+            ->name('admin.index');
+    });
+
+//route role 1 = operator surat
+Route::prefix('operator-surat')
+    ->middleware('auth','role:1')
+    ->group(function(){
+        //operator-surat dashboard
+        Route::get('/', [OperatorSuratController::class,'index'])
+            ->name('operator-surat.index');
+    });
+
+//route role 2 = operator-kepegawaian 
+Route::prefix('operator-kepegawaian')
+    ->middleware('auth','role:2')
+    ->group(function(){
+        //operator-kepegawaian dashboard
+        Route::get('/', [OperatorKepegawaianController::class,'index'])
+            ->name('operator-kepegawaian.index');
+    });
+
+
+
